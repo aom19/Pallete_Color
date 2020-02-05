4279411698eeeba0a337547a8d66ab7e10e95be3
aom19 <39848284+aom19@users.noreply.github.com>
2020-02-05 11:11:19 +0200

    added palette

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React ,{Component} from 'react';
+
+
+class Palette extends Component{
+	constructor(props){
+		super(props);
+
+	}
+	render(){
+		return(
+			<div>
+				<h1> Palette </h1>
+
+			</div>
+		);
+	}
+}
+export default Palette;
