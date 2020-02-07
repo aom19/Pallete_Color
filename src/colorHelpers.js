c768ff8440ccd9bebc45c5688bc730f59e33a6c1
sub_zero <ostapovici.anton@gmail.com>
2020-02-07 19:56:10 +0200

    Style the slider

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -3,7 +3,7 @@
 import chroma from "chroma-js";
 
 
-const levels=[50,100,200,3000,400,500,600,700,800,900];
+const levels=[50,100,200,300,400,500,600,700,800,900];
 
 
 function getRange(hexColor){
